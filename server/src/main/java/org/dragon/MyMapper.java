fa9cde3d4c756deecb0dedec05c664ce3a804a4d
dragonHandsome <61896951+dragonHandsome@users.noreply.github.com>
2020-06-19 14:15:28 +0800

    Update MyMapper.java
    。。。

--- server/src/main/java/org/dragon/MyMapper.java
+++ server/src/main/java/org/dragon/MyMapper.java
@@ -7,7 +7,6 @@ import tk.mybatis.mapper.common.MySqlMapper;
 
 import java.util.List;
 
-//这个东西不能被扫描 奇葩bug没注解依然没扫描 引起service 2个bean bug
 public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T>, IdsMapper<T>, ConditionMapper<T> {
 
 
